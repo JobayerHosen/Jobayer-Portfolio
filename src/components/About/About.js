5f706579ed3767f60c7e8c64a94206c2ba54229f
Jobayer Hosen <juba2732@gmail.com>
2026-03-01 17:43:37 +0600

    Update About component styles and enhance image loading effect

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -33,8 +33,8 @@ const About = () => {
 
           <Row className="about-hero">
             <Col xs={12} md={5} className="about-image-col">
-              <div className="about-image-wrapper">
-                {personal?.image ? <Image src={personal.image} alt="Jobayer Hosen" /> : <></>}
+              <div className={`about-image-wrapper ${loading ? "loading" : ""}`}>
+                {personal?.image ? <Image src={personal.image} /> : <></>}
                 <div className="about-image-overlay"></div>
               </div>
             </Col>
